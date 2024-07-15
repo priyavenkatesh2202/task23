cef9e8532672d676ba0f0c7f2d949f9bf382a8e2
priyavenkatesh2202 <priyavenkatesh2202@gmail.com>
2024-07-15 22:58:16 +0530

    Update Jdbc.java

--- Jdbc.java
+++ Jdbc.java
@@ -14,13 +14,15 @@ public class Jdbc {
 	public static void main (String args[])
 	
 	{
-	
+// 	create database connection	
   String db_url = "jdbc:mysql://localhost:3306";
   String user = "root";
   String password ="root";
-  
+		
+// establish the connection  
+		
   try {
-  		
+ 		 // check whether the connection is successful		
   		Connection connect = DriverManager.getConnection(db_url,user,password);
   		
   		
@@ -34,9 +36,11 @@ public class Jdbc {
   		{
   			System.out.println("connection not established");
   		}
+// to create database, to create table and insert values to the table
+			
+	// create the statement	  		
   		
-  		
-  		Statement st = connect.createStatement();
+  	Statement st = connect.createStatement();
   		
   	String createDB = "create database db_empl";
   	
@@ -46,37 +50,38 @@ public class Jdbc {
   	String insert = "insert into db_empl values(101,'jenny',25,10000),(102,'jacky',30,20000),(103,'joe',20,40000),(104,'john',40,80000),(105,'shameer',25,90000)";
   		
   	
-  		String select = "select* from db_empl";
-  		
+  	String select = "select* from db_empl";
+
+	  
+  // execute the statements	
   	st.execute(createDB);
   	st.execute(use);
   	st.execute(createTable);
   	st.executeUpdate(insert);
+	  
+  // to select the data		
+        ResultSet result = st.executeQuery(select);
   		
-  		ResultSet result = st.executeQuery(select);
-  		
-  		while(result.next()) {
-  			System.out.println(result.getInt("empcode")+" "+result.getString("empname")+" "+result.getInt("empage")+" "+result.getInt("esalary"));
-  			
-  			
-  		}
+  // iterate the result		
+	  while(result.next()) 
+	  {
+  	   System.out.println(result.getInt("empcode")+" "+result.getString("empname")+" "+result.getInt("empage")+" "+result.getInt("esalary"));
+  	  }
+// close the connection	  
   	connect.close();
-  	
-  	
-  }
+   }
+	  
+// to handle the sql exception	  
   catch (SQLException e)
   
   {		
 	  e.printStackTrace();
   }
 		
-
+	}
+}
   	    
 		    	
 		        
-		
-	}	        
-		        
-	}
 
 
